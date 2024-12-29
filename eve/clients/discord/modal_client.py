57e7e028e4118cdfd8d08a27e557d28351b61011
Jonathan Miller <jon@jmill.dev>
2024-12-29 13:01:25 -0500

    revert changes to discord

--- eve/clients/discord/modal_client.py
+++ eve/clients/discord/modal_client.py
@@ -1,24 +1,10 @@
-import os
 import modal
 
 from eve.clients.discord.client import start as discord_start
 
-db = os.getenv("DB", "STAGE").upper()
-print("THE DB IS", db)
-if db not in ["PROD", "STAGE"]:
-    raise Exception(f"Invalid environment: {db}. Must be PROD or STAGE")
-app_name = "client-discord-prod" if db == "PROD" else "client-discord-stage"
-env_file = ".env" if db == "PROD" else ".env.STAGE"
-
-if os.path.exists(env_file):
-    print(env_file)
-    print("ENV FILE EXISTS")
-else:
-    print("ENV FILE DOES NOT EXIST")
-    raise Exception(f"ENV FILE DOES NOT EXIST: {env_file}")
 
 app = modal.App(
-    name=app_name,
+    name="client-discord",
     secrets=[
         modal.Secret.from_name("client-secrets"),
         modal.Secret.from_name("eve-secrets", environment_name="main"),
@@ -27,10 +13,13 @@ app = modal.App(
 
 image = (
     modal.Image.debian_slim(python_version="3.11")
-    .env({"DB": db})
+    .env(
+        {
+            "DB": "STAGE",
+        }
+    )
     .apt_install("libmagic1", "ffmpeg", "wget")
     .pip_install_from_pyproject("pyproject.toml")
-    .pip_install("py-cord>=2.4.1")
     .copy_local_dir("../workflows", "/workflows")
 )
 
@@ -38,8 +27,4 @@ image = (
 @app.function(image=image, keep_warm=1, concurrency_limit=1, timeout=60 * 60 * 24)
 @modal.asgi_app()
 def modal_app() -> None:
-    discord_start(
-        env=env_file,
-        db=db,
-        local=False,
-    )
+    discord_start(env=".env")
