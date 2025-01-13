ba013fd95b27e3f8d3461b7bbf4cd11aa5c3d01f
aiaug <lindaxxxrow@gmail.com>
2025-01-13 13:36:51 +0900

    fix:this commit should fix the webhook_url

--- eve/tools/replicate_tool.py
+++ eve/tools/replicate_tool.py
@@ -162,8 +162,8 @@ class ReplicateTool(Tool):
         return prediction
 
 def get_webhook_url():
-    env = "api-prod" if os.getenv("DB") == "PROD" else "api-web3-stage"
-    dev = "-dev" if os.getenv("DB") == "STAGE" and os.getenv("MODAL_SERVE") == "1" else ""
+    env = "api-web3-prod" if os.getenv("DB") == "PROD" else "api-web3-stage"
+    dev = "-dev" if os.getenv("DB") == "WEB#-STAGE" and os.getenv("MODAL_SERVE") == "1" else ""
     webhook_url = f"https://edenartlab--{env}-fastapi-app{dev}.modal.run/update"
     return webhook_url
 
