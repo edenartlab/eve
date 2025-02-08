f6ad88384534798e4bcca7ab97c032bc29b94a45
genekogan <kogan.gene@gmail.com>
2025-02-08 11:41:46 -0800

    dummy build

--- eve/agent.py
+++ eve/agent.py
@@ -40,7 +40,7 @@ default_presets_flux = {
         "tip": "This should be your primary tool for making videos or animations. Only use the other video tools if specifically asked to or asked to make VJing material."
     },
     "reel": {
-        "tip": "This is a tool for making short films with vocals, music, and several video cuts. This can be used to make commercials, films, music videos, and other kinds of shortform content. But it takes a while to run, around 5 minutes."
+        "tip": "This is a tool for making short films with vocals, music, and multiple video cuts. This can be used to make commercials, films, music videos, and other kinds of shortform content. But it takes a while to run, around 5 minutes."
     },
     "news": {},
     "websearch": {},
