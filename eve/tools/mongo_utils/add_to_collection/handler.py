6b5159391480e8561590ba74b5a13b2230de7474
genekogan <kogan.gene@gmail.com>
2025-03-21 00:02:11 -0700

    change add_to_collection tool to work on id instead of search

--- eve/tools/mongo_utils/add_to_collection/handler.py
+++ eve/tools/mongo_utils/add_to_collection/handler.py
@@ -3,18 +3,20 @@ from ....tool import Tool
 
 
 async def handler(args: dict, user: str = None, agent: str = None):
-    query = args.get("query")
-    tool = Tool.load("search_collections")
-    results = await tool.async_run({
-        "query": query,
-        "results": 1
-    })
-    results = results.get("output", [])
+    # query = args.get("query")
+    # tool = Tool.load("search_collections")
+    # results = await tool.async_run({
+    #     "query": query,
+    #     "results": 1
+    # })
+    # results = results.get("output", [])
 
-    assert len(results) >= 1, "No collections found for this query"
+    # assert len(results) >= 1, "No collections found for this query"
 
+    # collection_id = results[0]["collection_id"]
+    collection_id = args.get("collection_id")
     creation_id = args.get("creation_id")
-    collection_id = results[0]["collection_id"]
+    
 
     collection = CreationsCollection.from_mongo(collection_id)
     collection.add_creation(creation_id)
