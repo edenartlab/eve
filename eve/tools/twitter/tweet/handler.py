a0d6ec2df9e60767af927aa221b7dbcf4b9ad457
genekogan <kogan.gene@gmail.com>
2025-03-21 17:06:23 -0700

    Remove agent arg in twitter tools

--- eve/tools/twitter/tweet/handler.py
+++ eve/tools/twitter/tweet/handler.py
@@ -4,12 +4,23 @@ from .. import X
 
 
 async def handler(args: dict, user: str = None, agent: str = None):
-    agent = Agent.from_mongo(args["agent"])
+    print("T1")
+    print(agent)
+    print(user)
+    print("Ggg")
+    agent = Agent.from_mongo(agent)
+    print("T2")
+    print(agent)
     # attempt to find a valid twitter deployment
+    print("T3")
     deployment = Deployment.load(agent=agent.id, platform="twitter")
+    print("T4")
+    print(deployment)
     if not deployment:
         raise Exception("No valid twitter deployments found")
+    print("T5")
     x = X(deployment)
+    print("T6")
     if args.get("images"):
         media_ids = [x.tweet_media(image) for image in args.get("images", [])]
         response = x.post(text=args.get("content") or "", media_ids=media_ids)
