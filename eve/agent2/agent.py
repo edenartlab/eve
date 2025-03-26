4a127fd208d0b174c7cccbf7a641612a9c82c19a
genekogan <kogan.gene@gmail.com>
2025-03-26 00:08:28 -0700

    use tools

--- eve/agent2/agent.py
+++ eve/agent2/agent.py
@@ -12,7 +12,7 @@ from datetime import datetime
 from dotenv import dotenv_values
 from pydantic import SecretStr, Field, BaseModel, ConfigDict
 
-from ..tool import Tool, BASE_TOOLS
+from ..tool import Tool, BASE_TOOLS, FLUX_LORA_TOOLS, SDXL_LORA_TOOLS
 from ..mongo import Collection, get_collection
 from ..user import User, Manna
 from ..models import Model
@@ -166,6 +166,61 @@ class Agent(User):
             schema["tools"].update({
                 k: {} for k in BASE_TOOLS if k not in schema["tools"]
             })
+
+        models = schema.get("models") or ([
+            {"lora": schema.get("model"), "use_when": "This is your default model."}] 
+            if schema.get("model") else []
+        )
+        for m in models:
+            m["doc"] = Model.from_mongo(m["lora"])
+
+        if models:
+            base_models = [
+                {
+                    "type": "flux-dev", 
+                    "models": [m for m in models if m["doc"].base_model == "flux-dev"],
+                    "tools": [t for t in schema["tools"].keys() if t in FLUX_LORA_TOOLS]
+                },
+                {
+                    "type": "sdxl", 
+                    "models": [m for m in models if m["doc"].base_model == "sdxl"],
+                    "tools": [t for t in schema["tools"].keys() if t in SDXL_LORA_TOOLS]
+                }
+            ]
+
+            for base_model in base_models:
+                model_list, tools_list = base_model["models"], base_model["tools"]
+                
+                if tools_list and model_list:
+                    if len(model_list) == 1:
+                        tip = f'Only use "{base_model["type"]}" models. Set the "lora" argument to the ID of the default lora (ID: {str(model_list[0]["lora"])}, Name: "{model_list[0]["doc"].name}", Description: "{model_list[0]["doc"].lora_trigger_text}"), if the following conditions are true: "{model_list[0]["use_when"]}"). If no lora is desired, leave this blank. If a different lora is desired, use its ID instead.'
+                        default_lora = model_list[0]["doc"].id
+                    else:
+                        models_info = " | ".join([
+                            f'ID: {m["lora"]}, Name: "{m["doc"].name}", Description: "{m["doc"].lora_trigger_text}", Use When: "{m["use_when"]}"' 
+                            for m in model_list
+                        ])
+                        tip = f'Only use "{base_model["type"]}" models. You are can use the following loras under the "Use When" circumstances: {models_info}. To use no lora, leave the "lora" argument blank.'
+                        default_lora = model_list[0]["doc"].id
+                        for model in model_list:
+                            if "default" in model["use_when"].lower():
+                                default_lora = model["doc"].id
+                                break
+                    
+                    print("732647246723 !!!", default_lora)
+                         
+                    tip += " If you use a lora, make sure to refer to it in the prompt using its exact Name. Avoid restating the Description in the prompt as it's implicit in the lora already and is redundant."
+
+                    # Update all related tools with the tip
+                    for tool in tools_list:
+                        schema["tools"][tool] = {
+                            "parameters": {
+                                "lora": {
+                                    "tip": tip, 
+                                    "default": str(default_lora)
+                                }
+                            }
+                        }
 
         return schema
 
