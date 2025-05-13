9e2f3399aff649dd48928e39a01b322702bcb4f7
Jonathan Miller <jon@jmill.dev>
2025-05-13 11:37:54 -0400

    these caches bro

--- eve/clients/discord/gateway.py
+++ eve/clients/discord/gateway.py
@@ -556,10 +556,7 @@ class DiscordGatewayClient:
         thread = self.known_threads[thread_key]
 
         # Lookup user
-        
-        if user_id not in self.known_users:
-            self.known_users[user_id] = User.from_discord(user_id, username)
-        user = self.known_users[user_id]
+        user = User.from_discord(user_id, username)
 
         return thread, user
 
