333f1e0b78ab34806b819f1ee63a33c8daf1709d
genekogan <kogan.gene@gmail.com>
2025-08-27 17:20:39 -0400

    veo3 image-to-video

--- eve/tools/google/veo3/handler.py
+++ eve/tools/google/veo3/handler.py
@@ -1,10 +1,6 @@
 from .. import veo_handler
 
 async def handler(args: dict, user: str = None, agent: str = None):
-    if "image" in args:
-        print("Warning: input image is not supported for veo3 yet")
-        args.pop("image", None)
-    
     return await veo_handler(
         args, 
         model="veo-3.0-generate-preview"
