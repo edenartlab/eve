a00bac16439efc3a7233905b01a8599fabd498bf
genekogan <kogan.gene@gmail.com>
2025-10-07 01:40:51 -0400

    lookup cli

--- eve/cli/__init__.py
+++ eve/cli/__init__.py
@@ -5,6 +5,7 @@ from .start_cli import start, api
 from .upload_cli import upload
 from .export_cli import export
 from .deploy_cli import deploy, configure, stop
+from .lookup_cli import lookup
 
 
 @click.group()
@@ -22,5 +23,6 @@ cli.add_command(export)
 cli.add_command(deploy)
 cli.add_command(stop)
 cli.add_command(configure)
+cli.add_command(lookup)
 
 __all__ = ["deploy", "start"]  # Add other CLI commands as needed
